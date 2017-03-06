513d74554b13ce5c745417ef8bbd0862f9454e2f
KM <gk77m@naver.com>
2017-03-06 20:54:10 +0900

    added output "Tell her world"

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello world");
 		System.out.println("Tell your world");
+		System.out.println("Tell her world");
 	}
 }
 
